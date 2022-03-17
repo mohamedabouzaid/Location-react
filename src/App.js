3d8f3ffbdb0a45cd94491a6f0e7b8218fd2a4882
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-17 20:29:53 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import{BrowserRouter,Route,Routes} from'react-router-dom'
+import Users from'./user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+const App=()=> {
+ 
+ 
+  return(
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Users />} />
+    <Route path="/places/new" element={< NewPlace />} />
+    <div>hello</div>
+    <Route path="*" element={<div> 404 page</div>} />
+    </Routes>
+    </BrowserRouter>
+    
+    
+    
+    </>
+  ) 
+  
 }
 
 export default App;
