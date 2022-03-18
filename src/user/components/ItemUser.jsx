7d6eda86937822c62bbb56cd2d83f2dc3c788763
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-18 15:08:44 +0200

    add folder UI

--- src/user/components/ItemUser.jsx
+++ src/user/components/ItemUser.jsx
@@ -9,14 +9,15 @@ const ItemUser = (props) => {
 
   return (
     <>
-      <li className='user-item'>
      
-          <Card className='user-item__content'>
+     
+          <Card>
+          <li className='user-item user-item__content'>
               <Link to ={`/${id}/place`}>
             <div className='user-item__image'>
               <Avatar image={image} alt={name} />
             </div>
-            <div className='user-item__info'>
+            <div className='item__info'>
               <h2>{name}</h2>
               <h3>
                 {loction}
@@ -24,9 +25,10 @@ const ItemUser = (props) => {
               </h3>
             </div>
             </Link>
+            </li>
           </Card>
         
-      </li>
+     
     </>
   );
 };
