46de49da6b09baa5db5d4d5fba14531c181ec463
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-19 01:12:21 +0200

    add model

--- src/places/components/ItemPlaces.jsx
+++ src/places/components/ItemPlaces.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import {useState} from 'react';
 import Card from '../../shared/components/UIElements/Card'
 import './ItemPlaces.css'
+import Button from '../../shared/components/ButtonEl/Button'
+import Modle from '../../shared/components/UIElements/Modal'
 
 const ItemPlaces = (props) => {
     const {place}=props
-  
-    return (
+  const [IsOpenModel,setOpenModel]=useState(false);
+  const clickModelHandler=()=>{setOpenModel(true)}
+  const cancleModelHandler=()=>{setOpenModel(false)}
+    return (<>
+
+      { IsOpenModel &&<Modle 
+      show={IsOpenModel}
+      onCancel={cancleModelHandler}
+      header={place.address}
+      contentClass='place-item__modal-content'
+      footerClass='place-item__modal-actions'
+      footer={<button onClick={cancleModelHandler}>Close</button>}>
+      <div className='map-container'>Mapis here</div>
+      
+      </Modle> }
         <li className='place-item'>
             <Card className='place-item'>
                 <div className='place-item__image'> 
@@ -17,12 +32,12 @@ const ItemPlaces = (props) => {
                     <p>{place.description}</p>
                 </div>
                 <div className='place-item__actions'>
-                    <button>View On Map</button>
-                    <button>Edit</button>
-                    <button>Delete</button>
+                    <Button onClick={clickModelHandler} inverse>View On Map</Button>
+                    <Button to={`/place/${place.id}`}>Edit</Button>
+                    <Button danger>Delete</Button>
                 </div>
             </Card>
-        </li>
+        </li></>
       
     );
 };
